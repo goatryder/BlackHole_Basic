ca2fd26de8a2699f22d85c84a5c6df3642443d47
goatryder <nova9ok@gmail.com>
2020-12-09 12:53:00 +0200

    patrolling work in progress

---
+++ Source/FPSGame/Public/FPSAIGuardController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "FPSAIGuardController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSGAME_API AFPSAIGuardController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
